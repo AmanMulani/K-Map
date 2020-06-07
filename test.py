8876c31df194f988cc2876c77baf6a7cb1fa312e
AmanMulani <amanmulani369@gmail.com>
2020-06-07 18:46:25 +0530

    Added some calculations feature to test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 def main():
-  pass
+  print('Here we will perform some basic operations. Operations of a calculator.')
+  a = 1
+  b = 2
+  c = 2**10 - 3
+  print(a+b)
+  print(a*c)
+  print(c/b)
   
 main()
