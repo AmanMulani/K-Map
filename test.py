41cd0c7d72cdefd10f3282ae33c7036f5f6d5e29
AmanMulani <amanmulani369@gmail.com>
2020-06-07 20:50:27 +0530

    Cleared a commit issue.

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 def main():
   print('Here we will perform some basic operations. Operations of a calculator.')
-  a = 1
-  b = 2
+  a = 141
+  b = 2465
   c = 2**10 - 3
-  print('Additions of two numbers: ',a+b)
-  print('Multiplication of two numbers: ', a*c)
-  print('Division of two numbers: ', c/b)
-  print('No Substraction feature yet.')
-    
+  print('Addition of {0} and {1} : {2}'.format(a, b, a+b))
+  print('Multiplication of {0} and {1} : {2} '.format(a, c, a*c))
+  print('Division of {0} and {1}: {2} '.format(c, b, c/b))
+
+  
 main()
